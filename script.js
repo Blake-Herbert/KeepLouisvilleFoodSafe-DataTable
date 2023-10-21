ac3192deae3e24be7000ffb9ec91a5a19b20c21e
Blake Herbert <Bwherb1@gmail.com>
2023-10-20 21:23:21 -0400

    JSON response now shows as a table in the html

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
+// Define the API URL
 const apiUrl = "https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_Inspection_Violations_of_Failed_Restaurants/FeatureServer/0/query?where=1%3D1&outFields=InspectionDate,premise_name,Insp_Viol_Comments&outSR=4326&f=json";
 
-// Make a GET request to the API
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json(); // Parse the JSON response
-  })
-  .then(data => {
-    // Handle the JSON response here
-    console.log(data);
+// Function to fetch and populate the table
+function populateTable() {
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const tableBody = document.querySelector("#dataTable tbody");
 
-    // Access and process each JSON object individually
-    data.features.forEach(feature => {
-      const inspectionDate = feature.attributes.InspectionDate;
-      const premiseName = feature.attributes.premise_name;
-      const comments = feature.attributes.Insp_Viol_Comments;
+            data.features.forEach(feature => {
+                const attributes = feature.attributes;
+                const inspectionDate = attributes.InspectionDate || "N/A";
+                const premiseName = attributes.premise_name || "N/A";
+                const violationComments = attributes.Insp_Viol_Comments || "N/A";
 
-      // Do something with each object, e.g., display it on the page
-      console.log(`Date: ${inspectionDate}, Premise: ${premiseName}, Comments: ${comments}`);
-    });
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the fetch.
-    console.error('There was a problem with the fetch operation:', error);
-  });
+                const row = tableBody.insertRow();
+                const cell1 = row.insertCell(0);
+                const cell2 = row.insertCell(1);
+                const cell3 = row.insertCell(2);
+
+                cell1.textContent = inspectionDate;
+                cell2.textContent = premiseName;
+                cell3.textContent = violationComments;
+            });
+        })
+        .catch(error => console.error("Error fetching data: " + error));
+}
+
+// Call the function to populate the table
+populateTable();
