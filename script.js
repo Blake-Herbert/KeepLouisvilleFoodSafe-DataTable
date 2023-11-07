a8e46f5b9472f23339c4d9c1c3c1271e093f12bc
Blake Herbert <Bwherb1@gmail.com>
2023-11-07 16:19:46 -0500

    Added Premise Street to datatable and updated apiurl to get the new information

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Define the API URL
-const apiUrl = "https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_Inspection_Violations_of_Failed_Restaurants/FeatureServer/0/query?where=1%3D1&outFields=InspectionDate,premise_name,Insp_Viol_Comments&outSR=4326&f=json";
+const apiUrl = "https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_Inspection_Violations_of_Failed_Restaurants/FeatureServer/0/query?where=1%3D1&outFields=InspectionDate,premise_name,premise_adr1_street,Insp_Viol_Comments&outSR=4326&f=json";
 
 function formatDate(timestamp) {
     if (timestamp) {
         const date = new Date(timestamp);
-        return date.toDateString(); // You can customize the date format as needed
+        return date.toDateString();
     }
     return "N/A";
 }
@@ -21,15 +20,18 @@ function populateTable() {
                 const inspectionDate = formatDate(attributes.InspectionDate) || "N/A";
                 const premiseName = attributes.premise_name || "N/A";
                 const violationComments = attributes.Insp_Viol_Comments || "N/A";
+                const premise_street = attributes.premise_adr1_street || "N/A";
 
                 const row = tableBody.insertRow();
                 const cell1 = row.insertCell(0);
                 const cell2 = row.insertCell(1);
                 const cell3 = row.insertCell(2);
+                const cell4 = row.insertCell(3);
 
                 cell1.textContent = inspectionDate;
                 cell2.textContent = premiseName;
-                cell3.textContent = violationComments;
+                cell3.textContent = premise_street;
+                cell4.textContent = violationComments;
             });
         })
         .catch(error => console.error("Error fetching data: " + error));
